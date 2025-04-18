982b47ace026eb24448ceadd52dc2d391a575a28
Rithwik Babu <rithwikbabu2020@gmail.com>
2025-04-18 13:01:12 -0500

    forgot to push this (0.0.13)

--- hawk_sdk/core/common/utils.py
+++ hawk_sdk/core/common/utils.py
@@ -8,14 +8,16 @@ from hawk_sdk.core.common.constants import PROJECT_ID
 
 
 def get_bigquery_client() -> bigquery.Client:
-    if 'GOOGLE_APPLICATION_CREDENTIALS' in os.environ:
-        # Initialize the client using the json file in the environment variable
-        return bigquery.Client(project=PROJECT_ID)
-
-    else:
-        # Load the service account credentials from the JSON string
-        service_account_json = os.environ.get('SERVICE_ACCOUNT_JSON')
+    service_account_json = os.environ.get('SERVICE_ACCOUNT_JSON')
+    if service_account_json:
+        # Use credentials provided in SERVICE_ACCOUNT_JSON
         credentials = service_account.Credentials.from_service_account_info(
             json.loads(service_account_json)
         )
         return bigquery.Client(project=PROJECT_ID, credentials=credentials)
+    else:
+        # Rely on Application Default Credentials (ADC),
+        # which will automatically use GOOGLE_APPLICATION_CREDENTIALS if set,
+        # or use the built-in credentials if running in GCP.
+        return bigquery.Client(project=PROJECT_ID)
+
