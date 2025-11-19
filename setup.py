614dc1949320fecd1b36bd908bba09fa50ee4e3d
Rithwik Babu <rithwikbabu2020@gmail.com>
2025-11-19 12:45:52 -0600

    (0.0.16) (#9)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hawk-sdk',
-    version='0.0.13',
+    version='0.0.16',
     packages=find_packages(),
     install_requires=[
         'google-cloud-bigquery',
